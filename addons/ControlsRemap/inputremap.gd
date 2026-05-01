a45eef58c41c422812cc4942a178f7bbdb5e73a2
kobewi <kobewi4e@gmail.com>
2026-05-01 14:48:33 +0200

    Improve plugin

--- addons/ControlsRemap/inputremap.gd
+++ addons/ControlsRemap/inputremap.gd
@@ -1,9 +1,12 @@
 @tool
 extends EditorPlugin
 
+const SETTING_NAME = "addons/ControlsRemap/action_list"
+
 func _enter_tree() -> void:
-	if not ProjectSettings.has_setting("addons/ControlsRemap/action_list"):
-		var defaults: Array[StringName]
-		defaults.assign([&"ui_up", &"ui_down", &"ui_left", &"ui_right", &"ui_accept", &"ui_cancel"])
-		ProjectSettings.set_setting("addons/ControlsRemap/action_list", defaults)
-	ProjectSettings.add_property_info({ "name": "addons/ControlsRemap/action_list", "type": TYPE_ARRAY, "hint": PROPERTY_HINT_TYPE_STRING, "hint_string": "21:StringName" })
+	var defaults: Array[StringName] = [&"ui_up", &"ui_down", &"ui_left", &"ui_right", &"ui_accept", &"ui_cancel"]
+	if not ProjectSettings.has_setting(SETTING_NAME):
+		ProjectSettings.set_setting(SETTING_NAME, defaults)
+	
+	ProjectSettings.set_initial_value(SETTING_NAME, defaults)
+	ProjectSettings.add_property_info({ "name": SETTING_NAME, "type": TYPE_ARRAY, "hint": PROPERTY_HINT_TYPE_STRING, "hint_string": "21/43:show_builtin" })
